62a0e9b0e637e9e0fcbf77b806ef564f3956ebb1
Cesar Mazzaro <cesar_mazzaro1@hotmail.com>
2021-09-27 17:01:50 -0300

    Estilização da Pagina New

---
+++ src/components/Picker/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const PickerView = styled.View`
+    margin-top: 20px;
+    background-color: #FFF;
+    align-items: center;
+    width: 90%;
+    padding: 5px;
+    height: 50px;
+    justify-content: center;
+    border-radius: 10px;
+`;
--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+
+import Header from '../../components/Header';
+import Picker from '../../components/Picker';
+import {
+  Background,
+  Input,
+  SubmitButton,
+  SubmitText
+}
+  from './styles';
 
 export default function New() {
- return (
-   <View>
-       <Text>New</Text>
-   </View>
+  const [valor, setValor] = useState('');
+  const [tipo, setTipo] = useState('receita');
+
+  const closeKeyboard = () => Keyboard.dismiss();
+  const getValor = text => setValor(text);
+
+  return (
+    <TouchableWithoutFeedback onPress={closeKeyboard}>
+      <Background>
+        <Header />
+
+        <SafeAreaView style={{ alignItems: 'center' }} >
+          <Input
+            placeholder='Valor desejado'
+            keyboardType='numeric'
+            returnKeyType='next'
+            onSubmitEditing={closeKeyboard}
+            value={valor}
+            onChangeText={getValor}
+          />
+
+          <Picker onChange={setTipo} tipo={tipo} />
+
+          <SubmitButton>
+            <SubmitText>Registrar</SubmitText>
+          </SubmitButton>
+
+        </SafeAreaView>
+      </Background>
+    </TouchableWithoutFeedback>
+
   );
 }
